f167df12c7814371233ba119d03f3db39dc8edbe
AnjanaTharanga24 <anjanatharanga24@gmail.com>
2026-05-03 07:34:53 +0000

    new updates

--- product-service/src/main/java/com/example/productservice/model/Product.java
+++ product-service/src/main/java/com/example/productservice/model/Product.java
@@ -18,19 +18,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
 
-    @Column(name = "amazon_url")
-    private String amazonUrl;
 
-    @Column(name = "amazon_price")
-    private BigDecimal amazonPrice;
 
-    @Column(name = "my_price")
-    private BigDecimal myPrice;
-
-    private BigDecimal profit;
-
-    @Column(name = "shipping_charge")
-    private BigDecimal shippingCharge;
 }
